6b542f59a1f209170cdc33ab496d6b47ac102de6
Sahil-DigitalOrbit <sahil.singh@digitalorbit.online>
2024-12-13 22:13:47 +0530

    Header Updated

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -23,7 +23,7 @@ const Homepage = () => {
               className="flex flex-col gap-5"
             >
               <p className="sm:max-w-screen-lg max-w-md font-normal text-gray-600 text-3xl m-auto">
-                Tea Bag <strong>Unleash your imagination</strong>{" "}
+                Tea Bag 3D Component <strong>Unleash your imagination</strong>{" "}
                 and define your own style.
               </p>
 
